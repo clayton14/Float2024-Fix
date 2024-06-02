ba6c24be350407b20d757556da66b361a9612475
clayton14 <claytoneasley77@gmail.com>
2024-06-02 17:41:02 -0400

    Add main dir

---
+++ main/include/pin_settings.h
@@ -0,0 +1,4 @@
+#define LED                 (15)
+#define STEP_PIN            (2)    // The step pin
+#define DIR_PIN             (4)   // the dir pin
+#define ENABLE              (32)   // the dir pin
---
+++ main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "pin_settings.h"
+
+//Tag for logging
+static const char *TAG = "main";
+
+
+void app_main(void)
+{
+
+}
