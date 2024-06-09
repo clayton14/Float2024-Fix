c7b173d37abcf63b6078043f0d2ff79d93801857
clayton14 <claytoneasley77@gmail.com>
2024-06-08 22:58:40 -0400

    Run webserver
      run the Webserver with FreeRTOS

--- main/main.c
+++ main/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <freertos/FreeRTOS.h>
+
 #include "esp_log.h"
-#include "pin_settings.h"
+#include "webserver.h"
+#include "config.h"
 
 //Tag for logging
 static const char *TAG = "main";
@@ -8,5 +11,22 @@ static const char *TAG = "main";
 
 void app_main(void)
 {
+
+// TODO Run webserver
+    void *param = NULL; 
+    TaskHandle_t stepper_init_task = NULL;
+    TaskHandle_t ws_task = NULL;
+
+    xTaskCreate(ws_run, "WEBSERVER", 3584, param, 1, &ws_task);
+    // xTaskCreate(stepper_init, "STEPPER", 3584, param, 1, &stepper_init_task);
+
+    configASSERT(ws_task);
+    //configASSERT(stepper_init_task);
+    while(1)
+    vTaskDelay(10);
+    
+// TODO impliment pressure sensor https://github.com/bluerobotics/BlueRobotics_MS5837_Library/blob/master/src/MS5837.cpp
+// TODO log and save data to CSV file 
+// send data to website
 
 }
